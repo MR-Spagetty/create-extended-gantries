a2ce3c5d53a45d64907a58dd4f7933e7d5fa47c3
MR-Spagetty <54694556+MR-Spagetty@users.noreply.github.com>
2026-06-13 14:48:09 +1200

    Fixed rendering, always remeber to initialise your registration classes kids

--- src/main/java/com/spag/extended_gantries/ExtendedGantries.java
+++ src/main/java/com/spag/extended_gantries/ExtendedGantries.java
@@ -4,21 +4,11 @@ import com.mojang.logging.LogUtils;
 import com.simibubi.create.foundation.data.CreateRegistrate;
 import com.spag.extended_gantries.registry.BlockEntityRegistry;
 import com.spag.extended_gantries.registry.BlockRegistry;
+import com.spag.extended_gantries.registry.PartialModels;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -27,9 +17,6 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 
@@ -92,6 +79,7 @@ public class ExtendedGantries
             // Some client setup code
             LOGGER.info("HELLO FROM CLIENT SETUP");
             LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+            PartialModels.init();
         }
     }
 }
--- src/main/java/com/spag/extended_gantries/registry/BlockEntityRegistry.java
+++ src/main/java/com/spag/extended_gantries/registry/BlockEntityRegistry.java
@@ -4,15 +4,14 @@ import com.simibubi.create.content.kinetics.base.KineticBlockEntityRenderer;
 import com.spag.extended_gantries.ExtendedGantries;
 import com.spag.extended_gantries.gantry_split.CustomVisual;
 import com.spag.extended_gantries.gantry_split.GantrySplitBlockEntity;
-import com.spag.extended_gantries.gantry_split.GantrySplitRenderer;
 import com.tterrag.registrate.util.entry.BlockEntityEntry;
 
 public interface BlockEntityRegistry {
     BlockEntityEntry<GantrySplitBlockEntity> GANTRY_SPLIT = ExtendedGantries.REGISTRATE
             .blockEntity("gantry_split", GantrySplitBlockEntity::new)
-            .visual(() -> CustomVisual::gantrySplit)
+            .visual(() -> CustomVisual::gantrySplit, false)
             .validBlocks(BlockRegistry.GANTRY_SPLIT)
-            .renderer(() -> GantrySplitRenderer::new)
+            .renderer(() -> KineticBlockEntityRenderer::new)
             .register();
 
     static void register() {
