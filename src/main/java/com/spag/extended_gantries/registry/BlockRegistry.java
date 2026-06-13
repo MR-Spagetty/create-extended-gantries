01850b2e385ab267389fa7a342d945251cfb4e7f
MR-Spagetty <54694556+MR-Spagetty@users.noreply.github.com>
2026-06-14 11:43:43 +1200

    Added Recipe for gantry_split

--- src/main/java/com/spag/extended_gantries/registry/BlockRegistry.java
+++ src/main/java/com/spag/extended_gantries/registry/BlockRegistry.java
@@ -1,17 +1,23 @@
 package com.spag.extended_gantries.registry;
 
 import com.simibubi.create.AllBlocks;
+import com.simibubi.create.AllItems;
 import com.simibubi.create.api.stress.BlockStressValues;
 import com.simibubi.create.content.kinetics.gantry.GantryShaftBlock;
 import com.simibubi.create.foundation.data.AssetLookup;
 import com.simibubi.create.foundation.data.SharedProperties;
 import com.spag.extended_gantries.ExtendedGantries;
+import com.spag.extended_gantries.Util;
 import com.spag.extended_gantries.gantry_split.GantrySplitBlock;
+import com.tterrag.registrate.providers.RegistrateRecipeProvider;
 import com.tterrag.registrate.util.entry.BlockEntry;
 
 import net.minecraft.core.Direction.AxisDirection;
+import net.minecraft.data.recipes.RecipeCategory;
+import net.minecraft.data.recipes.ShapedRecipeBuilder;
 import net.minecraft.world.level.material.MapColor;
 import net.minecraftforge.client.model.generators.ModelFile;
+import net.minecraftforge.common.Tags;
 
 public interface BlockRegistry {
     public static final BlockEntry<GantrySplitBlock> GANTRY_SPLIT = ExtendedGantries.REGISTRATE
@@ -35,7 +41,17 @@ public interface BlockRegistry {
                         .withExistingParent("block/" + c.getName() + "_" + partName + powered + flipped,
                                 existing.getLocation())
                         .texture("2", "create:block/gantry_shaft" + powered + flipped);
-            })).item()
+            })).item().recipe((c, p) -> {
+                ShapedRecipeBuilder.shaped(RecipeCategory.MISC, c.get(), 8)
+                .define('A', AllItems.ANDESITE_ALLOY)
+                .define('R', Tags.Items.DUSTS_REDSTONE)
+                .define('I', Tags.Items.NUGGETS_IRON)
+                .pattern(" A ")
+                .pattern("IRI")
+                .pattern(" A ")
+                .unlockedBy("has_gantry", RegistrateRecipeProvider.has(AllBlocks.GANTRY_SHAFT.get()))
+                .save(p, Util.asResource("crafting/"+c.getName()));
+            })
             .transform(com.simibubi.create.foundation.data.ModelGen.customItemModel("_", "block_single")).register();
 
     public static void register() {
